18a16debd97d0fb26f1f91896dd283cda109e453
Abhishek Thakur <officalabhishekthakur@gmail.com>
2023-02-03 20:56:19 +0530

    hamburger menu added for mobie version

---
+++ script.js
@@ -0,0 +1,9 @@
+var hamburgerMenu = document.getElementById("hamburgerMenu");
+function showMenu() {
+  hamburgerMenu.style.right = "0";
+  hamburgerMenu.style.display = "block";
+}
+function hideMenu() {
+  hamburgerMenu.style.right = "-200px";
+  hamburgerMenu.style.display = "none";
+}
